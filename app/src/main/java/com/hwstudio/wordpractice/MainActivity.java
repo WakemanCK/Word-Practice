b993b55b17ad1bc01c55c9a8e2a8e3b9b3ad68ab
Wakeman Chau <wakeman1029@yahoo.com.hk>
2020-12-23 01:34:30 +0800

    start

---
+++ app/src/main/java/com/hwstudio/wordpractice/MainActivity.java
@@ -0,0 +1,42 @@
+package com.hwstudio.wordpractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.UtteranceProgressListener;
+import android.view.View;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void clickPlay(View view){
+
+    }
+
+    public void initTTS(TextToSpeech getTTS, int getPitch, int getSpeechRate, int getLanguage, int getSoundVolume) {
+//        textToSpeech = getTTS;
+//        language = getLanguage;
+//        soundVolume = getSoundVolume;
+        float[] pitchFloat = {0.4f, 0.6f, 0.8f, 1f, 1.5f, 2f, 2.5f};
+        getTTS.setPitch(pitchFloat[getPitch]);
+        float[] speechRateFloat = {0.1f, 0.4f, 0.75f, 1f, 1.5f, 2f, 2.5f};
+        getTTS.setSpeechRate(speechRateFloat[getSpeechRate]);
+    }
+
+    public void speakString(TextToSpeech getTTS, Locale language, float soundVolume, String getString, UtteranceProgressListener utterance) {
+        getTTS.setOnUtteranceProgressListener(utterance);
+                        getTTS.setLanguage(language);
+        Bundle params = new Bundle();
+        params.putFloat(TextToSpeech.Engine.KEY_PARAM_VOLUME, soundVolume / 100f);
+        getTTS.speak(getString, TextToSpeech.QUEUE_ADD, params
+                , TextToSpeech.ACTION_TTS_QUEUE_PROCESSING_COMPLETED);
+    }
+}
